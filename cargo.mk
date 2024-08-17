0259cfaac047546bd53b5b73de2278daf569b452
randymcmillan <randymcmillan@protonmail.com>
2024-08-17 18:10:45 -0400

    make:cargo-i

--- cargo.mk
+++ cargo.mk
@@ -30,7 +30,7 @@ cargo-build:### 	cargo build
 cargo-i:cargo-install
 cargo-install:### 	cargo install --path jj
 	@. $(HOME)/.cargo/env
-	@cargo install --path . $(FORCE)
+	@cargo build --release $(FORCE)
 	@cargo install --path solarized $(FORCE)
 cargo-br:cargo-build-release### 	cargo-br
 ## 	cargo-br q=true
